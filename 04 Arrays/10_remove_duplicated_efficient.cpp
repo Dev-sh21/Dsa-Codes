ddab076a394669f7994912a9ed85081ccf7f1608
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2025-10-31 09:32:25 +0530

    naive approach to move zeres to the end

--- 04 Arrays/10_remove_duplicated_efficient.cpp
+++ 04 Arrays/10_remove_duplicated_efficient.cpp
@@ -14,7 +14,7 @@ int main(){
   int arr[]={2,4,4,5,6,6,6,7,7};
   int n=8;
   n=removeDuplicate(arr,n);
-  cout<<"after removall"<<endl;
+  cout<<"after removal"<<endl;
   for(int i=0;i<n;i++){
     cout<<arr[i]<<" ";
   }
---
+++ 04 Arrays/11_moving_zeroes_atEnd_naive.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void zerosAtEnd(int arr[],int n){
+  for(int i=0;i<n;i++){
+    if(arr[i]==0){
+      for(int j=i+1;j<n;j++){
+        if(arr[j]!=0) swap(arr[i],arr[j]);
+      }
+    }
+  }
+}
+int main(){
+  int arr[]={2,0,3,1,0,6,1,0};
+  int n=8;
+  zerosAtEnd(arr,n);
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+  
+
+  
+}
