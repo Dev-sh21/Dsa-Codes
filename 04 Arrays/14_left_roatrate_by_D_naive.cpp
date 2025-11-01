f616b648c722e80a5acc5cd5ec90d57a8f0fbcc7
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2025-11-01 15:31:12 +0530

    left rotated by d places naive approach

---
+++ 04 Arrays/14_left_roatrate_by_D_naive.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void leftRotateByOne(int arr[], int n){
+  int temp=arr[0];
+  for(int i=1;i<n;i++){
+    arr[i-1]=arr[i];
+  }
+  arr[n-1]=temp;
+}
+void leftRotatedByD(int arr[],int n,int d){
+  for(int i=0;i<d;i++){
+    leftRotateByOne(arr,n);
+  }
+}
+ int main(){
+  int arr[]={2,4,6,7,4,2};
+  int n=6;
+  int d=2;
+  leftRotatedByD(arr,n,d);
+  cout<<"the rotated array is "<<endl;
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  } 
+
+ }
