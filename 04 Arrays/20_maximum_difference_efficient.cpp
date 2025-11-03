95656bf7862b37e7361a464a232cdff107f3f5f2
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2025-11-03 21:46:06 +0530

    maximum diff efficient approach

---
+++ 04 Arrays/20_maximum_difference_efficient.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int maxDiff(int arr[], int n){
+  int res=arr[1]-arr[0];
+  int minDiff=arr[0];
+ for(int j=1;j<n;j++){
+  res=max(res,arr[j]-minDiff);
+  minDiff=min(minDiff,arr[j]);
+ }
+ return res;
+}
+int main(){
+  int arr[]={2,4,6,7,342,5};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  cout<<"the maximum difference is "<<maxDiff(arr,n)<<" ";
+}
