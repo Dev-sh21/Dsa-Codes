5d74be48e7b7ef96489ed474f5c83682cf4831ee
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2025-12-15 20:49:12 +0530

    intersection of 2 array naive

---
+++ 06 Sorting/08_insertionOf2SortedArray_naive.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void insection(int a[], int b[], int m, int n){
+  for(int i=0;i<m;i++){
+    if(i>0 && (a[i-1]==a[i])) continue;
+    for(int j=0;j<n;j++){
+     if(a[i]==b[j]){
+      cout<<a[i]<<" ";
+      break;
+    }
+  }
+  }
+
+}
+int main(){
+    int a[]={3,5,10,10,10,15,15,20};
+    int b[]={5,10,10,15,30};
+    int m=sizeof(a)/sizeof(a[1]);
+    int n=sizeof(b)/sizeof(b[1]);
+
+
+    insection(a,b,m,n);
+}
