d87b02fd405cf387ed2af8cc31876ad453e89e70
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2025-12-18 02:30:06 +0530

    quick sort using lomuto

---
+++ 06 Sorting/17_quickSortLomuto.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int partitionLomuto(int arr[], int l, int h){
+  int i=l-1;
+  int pivot=arr[h];
+  for(int j=l;j<h;j++){
+    if(arr[j]<pivot){
+      i++;
+      swap(arr[i],arr[j]);
+    }
+  }
+  swap(arr[i+1], arr[h]);
+  return i+1;
+}
+int qSort(int arr[], int l, int h){
+  if(l<h){
+    int p=partitionLomuto(arr,l,h);
+    qSort(arr,l,p-1);
+    qSort(arr,p+1,h);
+  }
+}
+
+int main()
+{
+  int arr[]={8,4,7,9,3,10,5};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  qSort(arr,0,n-1);
+  for(int x:arr)cout<<x<<" ";
+}
