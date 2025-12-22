cb99eb0db203c4ec84effa4d30c55e504d893a55
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2025-12-22 13:16:22 +0530

    array to vector added

--- 07 Matrix/04_arrayToPointer.cpp
+++ 07 Matrix/04_arrayToPointer.cpp
@@ -1,12 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <deque>
 using namespace std;
-int main(){
-  int m=3,n=2;
-  int *arr[m];
-  for(int i=0;i<m;i++){
-    arr[i]=new int[n];
-    for(int j=0;j<n;j++){
 
-    }
-  }
+
+int main()
+{	
+	int m = 3, n = 2;
+
+	int *arr[m];
+
+	for(int i = 0; i < m; i++)
+		arr[i] = new int[n];
+
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 2; j++)
+		{
+			arr[i][j] = 10;	
+
+			cout << arr[i][j] << " ";
+		}
+	}
+
+
+	return 0;
 }
