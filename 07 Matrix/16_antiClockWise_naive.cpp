467c66fdaf86efb5c75d70ece4880d234d4beccd
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2025-12-23 01:51:46 +0530

    naive approach to rotate matrix by 90 degree

--- 07 Matrix/16_antiClockWise_naive.cpp
+++ 07 Matrix/16_antiClockWise_naive.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <bits/stdc++.h> 
 #include <climits>
 #include <deque>
 using namespace std;
@@ -11,6 +12,19 @@ void transpose(int mat[n][n])
     for(int i = 0; i < n; i++)
 		for(int j = i + 1; j < n; j++)
 			swap(mat[i][j], mat[j][i]);
+			
+	for(int i = 0; i < n; i++)
+	{
+	    int low = 0, high = n - 1;
+	    
+	    while(low < high)
+	    {
+	        swap(mat[low][i], mat[high][i]);
+	        
+	        low++;
+	        high--;
+	    }
+	}
 }
 
 int main()
