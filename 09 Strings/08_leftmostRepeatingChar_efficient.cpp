97935ee102d01293277ebe9fc5286b38c40d6359
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-01-13 23:52:40 +0530

    efficient approach

---
+++ 09 Strings/08_leftmostRepeatingChar_efficient.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+const int CHAR=256;
+int leftMostRepeat(string &str){
+  bool visited[CHAR];
+  fill(visited, visited+CHAR, false);
+  int res=-1;
+  for(int i=str.length()-1;i>=0;i--){
+    if(visited[str[i]]) res=i;
+    else visited[str[i]]=true;
+  }
+  return res;
+}
+int main(){
+  string str="geeksforgeeks";
+  cout<<"the index of leftmost repeating character is\n";
+  cout<<leftMostRepeat(str);
+
+}
