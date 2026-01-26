f71ca092432ac2233629758517b07e4f1473841f
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-01-26 21:50:22 +0530

    reverse the word in a string

---
+++ 09 Strings/09_reverseWord.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void reverse(string s){
+  vector<string>temp;
+  string str="";
+
+  for(int i =0;i<s.length();i++){
+    if(s[i]==' '){
+      temp.push_back(str);
+      str="";
+    }else{
+      str += s[i];
+    }
+
+  }
+  temp.push_back(str);
+
+  for(int i=temp.size()-1;i>0;i--){
+    cout<<temp[i]<<" ";
+  }
+
+  cout<<temp[0]<<endl;
+
+
+
+}
+int main(){
+  string s="I like this program very much";
+  reverse(s);
+  return 0;
+}
