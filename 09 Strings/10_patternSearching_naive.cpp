13193d66f4fa7f38c537d7abf7c152f0d17926ac
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-01-29 00:07:55 +0530

    pattern searchinbg naive approach

---
+++ 09 Strings/10_patternSearching_naive.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void isPresent(string text, string pattern){
+  int n=text.length();
+  int m=pattern.length();
+
+  for(int i=0;i<=n-m;i++){
+    int j;
+    for(j=0;j<m;j++){
+      if(pattern[j]!=text[i+j]) break;   
+    }
+    if(j==m) cout<<i<<" ";
+    
+
+  }
+}
+int main(){
+  string text="abababcd";
+  string pattern="abab";
+  cout<<"All index numbers where pattern found:"<<" ";
+  isPresent(text,pattern);
+  return 0;
+}
